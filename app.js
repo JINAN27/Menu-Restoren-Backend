d89a658bb2a9d002f1364ca765ddbcbc42bf005e
jinan <muhamadmiftahul.27@gmail.com>
2024-12-27 10:42:40 +0700

    memberikan akses kepada front end untuk mengakses backend

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(cors({
-  origin: 'https://menu-restoran-front-end.vercel.app/', // Alamat frontend
+  origin: 'https://menu-restoran-front-end.vercel.app', // Alamat frontend
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
